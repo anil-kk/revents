d816b4b842da5e16b37a2a3e571a09508db316e9
Anil Kumar Kothapalli <anilkumar1988@gmail.com>
2018-10-19 11:10:19 +0200

    hot module replacement, webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootElement);
+}
+
+if (module.hot) {
+    module.hot.accept('./App.js', () => {
+        setTimeout(render);
+    })
+}
+
+render();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
